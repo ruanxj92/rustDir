4e3bf7360029ff2271480389b214a5205a09fe49
ruanUbuntuVM <ruan.xj92@gmail.com>
2022-10-01 20:53:43 +0800

    add match

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,10 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 fn main() {
     println!("guess a number ");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101);// i32
 
     println!("secrt_number is {secret_number}");
     let mut guess = String::new();
@@ -13,4 +14,13 @@ fn main() {
         .expect("cannot read line");
 
     println!("you guess:{}", guess);
+    //shadow same name
+    let guess:u32 = guess.trim().parse().expect("please input a number");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("you win"),
+
+    }
 }
