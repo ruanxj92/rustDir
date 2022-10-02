d8cab2f2f52bc9d2776ed9c65b56dfd2318a75e8
ruanUbuntuVM <ruan.xj92@gmail.com>
2022-10-02 18:58:18 +0800

    if let

--- option_example/src/main.rs
+++ option_example/src/main.rs
@@ -4,4 +4,17 @@ fn main() {
     let _some_char = Some('e');
 
     let _absent_number: Option<i32> = None;
+    if_let_example();
+}
+fn if_let_example(){
+    let v = Some(3u8);
+
+    match v{
+        Some(3) => println!("three");
+        _=>(),
+    }
+    //if let 处理单分支情况, 与上面的match等价
+    if let Some(3) = v{
+        println!("three");
+    }
 }
